83dba67634fea57a28f9594a11bdb16076b9c22d
Aqiu <1324236581@qq.com>
2023-03-09 10:58:16 +0800

    这是第一次提交

---
+++ src/main/java/com/atguigu/git/Main.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author AQiu
+ * @Create ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
